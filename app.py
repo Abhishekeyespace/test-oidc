b1c310d1f3272a712102fe4f151483b834f0e28b
abhishekeyespace <abhishek@eyespacelenses.com>
2022-12-15 11:23:18 +1030

    changd locale to CN

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def login():
         # print(MOCK_DB)
         # create a MOCK_DB entry for the user
        
-        MOCK_DB["1"] = {"email": email,"locale":"zh-cn" }
+        MOCK_DB["1"] = {"email": email,"locale":"zh-CN" }
         print(MOCK_DB)
         return render_template('home.html',email=email)
 
